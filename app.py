c33ce18fd44476897c3d9025b72f1009255025b0
Luis Enrique Quispe Paredes <7812078+luenqp@users.noreply.github.com>
2025-04-17 15:24:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ st.set_page_config(page_title="Descarga Nomás", page_icon="🎧")
 
 col1, col2 = st.columns([1, 4])
 with col1:
-    st.image("logo.png", width=120)
+    st.image("logo.png", width=100)
 with col2:
     st.markdown("<h1 style='margin-top: 5px;'>Descarga<br/>Nomás 🎶</h1>", unsafe_allow_html=True)
 
