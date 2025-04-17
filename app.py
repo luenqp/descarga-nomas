442949e06f0512047f9b2f47c22e3763341aeaf6
Luis Enrique Quispe Paredes <7812078+luenqp@users.noreply.github.com>
2025-04-17 15:30:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def descargar_mp3(url, carpeta_destino='descargas'):
 
 st.set_page_config(page_title="Descarga Nomás", page_icon="🎧")
 
-st.markdown("<h1 style='margin-top: 5px;'>🎧 Descarga<br/>Nomás 🎶</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='margin-top: 1px;'>🎧 Descarga<br/>Nomás 🎶</h1>", unsafe_allow_html=True)
 
 st.write("Descarga tu música MP3 de manera rápida y sencilla.")
 urls_input = st.text_area("🔗 Pega una o más URLs de YouTube (separadas por comas):", height=100)
